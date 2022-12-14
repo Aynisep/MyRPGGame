42e44f2e8549d5dd5046d784c4ae4b7adc850a50
Aynisep <119657073+Aynisep@users.noreply.github.com>
2022-12-14 13:34:41 +0100

    Add files via upload

---
+++ JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.myrpggame","l":"com.isep.rpg"},{"m":"com.example.myrpggame","l":"com.isep.utils"}];updateSearchResults();
